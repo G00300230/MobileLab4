29d72613ea61a2a5463da290904200e74931fca4
CUAN O CONCHUIR DILLON <G00300230@gmit.ie>
2019-02-13 15:46:38 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'data-binding-app';
+export class AppComponent 
+{
+  counter:number = 0;
+  hide:boolean = true;
+
+  buttonClicked()
+  {
+    this.counter++;
+    console.log("button clicked");
+  }
+
+  dbClick()
+  {
+    console.log("img clicked");
+    
+    if(this.hide == true)
+    {
+      this.hide = false;
+    }
+    else if(this.hide == false)
+    {
+      this.hide = true;
+    }
+
+  }
 }
